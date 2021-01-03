9ed8f42e0fff3c630f45984101c5db4c0b03563c
cconrad <584491+cconrad@users.noreply.github.com>
2021-01-03 20:31:03 +0100

    Add Mudlet

---
+++ manual/mudlet/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.mudlet.org/download/Mudlet-4.10.1-windows-installer.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'Mudlet*'
+
+  checksum      = '660558f9661e3aeb68b474787f2331a8940271df982f48794e60f659bf6ef5dd'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/silent'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
