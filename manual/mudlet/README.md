1eb3495068b54f5f6d52a3aa7d5361a5d60af0ca
cconrad <584491+cconrad@users.noreply.github.com>
2021-01-25 19:07:37 +0100

    Add Mudlet

---
+++ manual/mudlet/README.md
@@ -0,0 +1,22 @@
+﻿## About
+
+Mudlet is a quality [MUD](https://en.wikipedia.org/wiki/MUD) client, designed to take mudding to a new level.
+
+It’s a modern breed of a client on the gaming scene – with an intuitive user interface, a specially designed scripting framework, and a very fast text display. That, along with cross-platform capability and an open-source development model result in a very likable game client.
+
+## Key Features
+
+* Simple to use
+* Very, very fast
+* Active development
+* Big, thriving community
+* Powerful trigger engine
+* Rich Lua-based coding functionality (API)
+* Excellent script editor
+* 2D and 3D mapper with autowalk
+* Powerful and simple to use GMCP
+* Discord integration
+* Completely themable
+* Open-source and Free
+* Cross platform: runs on Windows, macOS and Linux
+* Full compatibility: run same scripts on any of the above
--- manual/xmind-2020/tools/chocolateyinstall.ps1
+++ manual/xmind-2020/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl3.xmind.net/XMind-2020-for-Windows-32bit-10.3.0-202012160334.exe'
-$url64      = 'https://dl3.xmind.net/XMind-2020-for-Windows-64bit-10.3.0-202012160243.exe'
+$url        = 'https://dl3.xmind.net/XMind-2020-for-Windows-32bit-10.3.1-202101070059.exe'
+$url64      = 'https://dl3.xmind.net/XMind-2020-for-Windows-64bit-10.3.1-202101070032.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
 
   softwareName  = 'XMind 10*'
 
-  checksum      = 'D819CF3B0D2A715CEA219EA7DDF428325944DD696D8F3CEA9A92EDA48A47A056'
+  checksum      = '2a0893610ce3bf5c15af81b832c3d5ec403059f35c523a3a19fcb67bd8878817'
   checksumType  = 'sha256'
-  checksum64    = '93F9F977C2D946F6907354E537AF4FBBA2CF6FDC2138E7A8A28FA4CFC058CEC9'
+  checksum64    = '6fd240b6127999f40cdb4c988131dc3318dbfcce5d2f2ceeef19a5005d247bf8'
   checksumType64= 'sha256'
 
   silentArgs   = '/allUsers /S'
