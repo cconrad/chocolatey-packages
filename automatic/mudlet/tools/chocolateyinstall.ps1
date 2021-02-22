d48e19b282bc91bc259dc6cdb813a81cec174403
cconrad <584491+cconrad@users.noreply.github.com>
2021-02-22 22:03:33 +0100

    Improve mudlet

--- automatic/mudlet/tools/chocolateyinstall.ps1
+++ automatic/mudlet/tools/chocolateyinstall.ps1
@@ -1,14 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = 'https://www.mudlet.org/download/Mudlet-4.10.1-windows-installer.exe'
-
-  softwareName  = 'Mudlet*'
 
   checksum      = '660558f9661e3aeb68b474787f2331a8940271df982f48794e60f659bf6ef5dd'
   checksumType  = 'sha256'
