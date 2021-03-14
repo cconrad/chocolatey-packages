e34791909046ab6a338f0ab4921ff3e110173df9
cconrad <584491+cconrad@users.noreply.github.com>
2021-03-14 18:48:57 +0100

    Update mudlet to 4.11.1

--- automatic/mudlet/tools/chocolateyinstall.ps1
+++ automatic/mudlet/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url           = 'https://www.mudlet.org/download/Mudlet-4.10.1-windows-installer.exe'
+  url           = 'https://www.mudlet.org/download/Mudlet-4.11.1-windows-installer.exe'
 
-  checksum      = '660558f9661e3aeb68b474787f2331a8940271df982f48794e60f659bf6ef5dd'
+  checksum      = '9a5b0b83a71f2f7180afb87263bab388a5bceaccb7f3e8380d3c20bf8c3802d0'
   checksumType  = 'sha256'
 
   silentArgs    = '/silent'
