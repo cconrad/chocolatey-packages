543ec7cef4af239d136ac1ebc4e430f00831377b
cconrad <584491+cconrad@users.noreply.github.com>
2021-05-30 02:33:48 +0200

    Update XMind

--- automatic/xmind-2020/tools/chocolateyinstall.ps1
+++ automatic/xmind-2020/tools/chocolateyinstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = 'https://dl3.xmind.net/XMind-2020-for-Windows-32bit-10.3.1-202101070059.exe'
-  url64bit      = 'https://dl3.xmind.net/XMind-2020-for-Windows-64bit-10.3.1-202101070032.exe'
+  url           = 'https://dl3.xmind.net/XMind-2021-for-Windows-32bit-11.0.0-202105270133.exe'
+  url64bit      = 'https://dl3.xmind.net/XMind-2021-for-Windows-64bit-11.0.0-202105270001.exe'
 
   softwareName  = 'XMind 10*'
 
-  checksum      = '2a0893610ce3bf5c15af81b832c3d5ec403059f35c523a3a19fcb67bd8878817'
+  checksum      = 'f6f7915ba6a7306d47c471c0190a4c0e8ef044e543ac1f5a216abef2c17e7a20'
   checksumType  = 'sha256'
-  checksum64    = '6fd240b6127999f40cdb4c988131dc3318dbfcce5d2f2ceeef19a5005d247bf8'
+  checksum64    = 'd2b3ab584c2fe676e7b32246d0e52fdb964c74745f36fffe5ff4a2590f854180'
   checksumType64= 'sha256'
 
   silentArgs   = '/allUsers /S'
