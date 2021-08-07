5d28723a9333893dc2a39a3fdb4336868f31962a
cconrad <584491+cconrad@users.noreply.github.com>
2021-08-07 15:46:40 +0000

    Add devcontainer configuration

--- setup/au_setup.ps1
+++ setup/au_setup.ps1
@@ -7,11 +7,11 @@ if ($PSVersionTable.PSVersion -lt $(New-Object System.Version("5.0.0.0"))) {
 $refreshenv = Get-Command refreshenv -ea SilentlyContinue
 if ($refreshenv -ne $null -and $refreshenv.CommandType -ne 'Application') {
   refreshenv # You need the Chocolatey profile installed for this to work properly (Choco v0.9.10.0+).
-} else {
-  Write-Warning "We detected that you do not have the Chocolatey PowerShell profile installed, which is necessary for 'refreshenv' to work in PowerShell."
 }
 
-Install-PackageProvider -Name NuGet -Force
+if ($null -eq (Get-PackageProvider NuGet)) {
+  Install-PackageProvider -Name NuGet -Force
+}
+
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module au -Scope AllUsers
-Get-Module au -ListAvailable | select Name, Version
