6d2fc52414a482df8dd10c107866567f697f7c76
cconrad <584491+cconrad@users.noreply.github.com>
2022-07-05 13:53:35 +0200

    Update XMind to v12.0.3

--- automatic/xmind-2020/tools/chocolateyinstall.ps1
+++ automatic/xmind-2020/tools/chocolateyinstall.ps1
@@ -4,13 +4,13 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url           = 'https://dl3.xmind.net/XMind-for-Windows-32bit-11.1.2-202111072106.exe'
-  url64bit      = 'https://dl3.xmind.net/XMind-for-Windows-64bit-12.0.2-202204260729.exe'
+  url64bit      = 'https://dl3.xmind.net/XMind-for-Windows-64bit-12.0.3-202206241736.exe'
 
-  softwareName  = 'XMind 10*'
+  softwareName  = 'XMind 12*'
 
   checksum      = '7099f476850e2a3a3d1a71dac1635af914412556dc3b1b3048fc68cec239d2cd'
   checksumType  = 'sha256'
-  checksum64    = '55AB592E08A83C716D706C978B73CF2F85E360FA189CB379F379BDA9853A72F9'
+  checksum64    = '3C2A264125E31A22A5B1D1B92E8D93671498B5F3D093BEBDAD2566362695C183'
   checksumType64= 'sha256'
 
   silentArgs   = '/allUsers /S'
