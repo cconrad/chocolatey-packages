646f97aa43c9edaf206fe9e084f38618672a3041
cconrad <584491+cconrad@users.noreply.github.com>
2023-10-13 13:20:26 +0200

    chore: update scrivener

--- manual/scrivener/tools/chocolateyinstall.ps1
+++ manual/scrivener/tools/chocolateyinstall.ps1
@@ -3,11 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = "EXE"
-  url            = "https://scrivener.s3.amazonaws.com/Scrivener-installer-x86.exe"
   url64bit       = "https://scrivener.s3.amazonaws.com/Scrivener-installer.exe"
-  checksum       = "F5FEABCCC56EC9C263A59F785A56999EFDB6754C1EBF3990B95A07F313E3EC70"
-  checksum64     = "C5FDC21C3761C1B7A4736D2509CDBA053881B25F091C6630261E9EB258A3B197"
-  checksumType   = "sha256"
+  checksum64     = "A9F6A14C20CF178D0EB36C8D7087E2EFEF2F03CE74BE79D54DE577EC6FDFA373"
   checksumType64 = "sha256"
   silentArgs     = "--mode unattended"
   validExitCodes = @(0)
