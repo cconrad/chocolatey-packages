cb68ebdb61319eec715959c010e538b03ce38455
cconrad <584491+cconrad@users.noreply.github.com>
2024-05-19 14:07:55 +0200

    Updated download URI for Mudlet v4.17.2

--- automatic/mudlet/tools/chocolateyinstall.ps1
+++ automatic/mudlet/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url           = 'https://www.mudlet.org/download/Mudlet-4.17.2-windows-installer.exe'
+  url           = 'https://www.mudlet.org/wp-content/files/Mudlet-4.17.2-windows-installer.exe'
 
   checksum      = '67cfbfb33c11a0c6b2295083c4c9b157dd68d4fb82e500709625e04722fe124f'
   checksumType  = 'sha256'
